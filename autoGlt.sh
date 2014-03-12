cd67ebb90f63cedb8ca696e2e9716795c704d898
David Davidson <david.davidson@logos.com>
2014-03-12 11:34:42 -0700

    Added support for GLTs that begin with &, not ?, after URLs that include previous parameters

--- autoGlt.sh
+++ autoGlt.sh
@@ -15,4 +15,7 @@ GLT="$SOURCE$MEDIUM$CONTENT$CAMPAIGN"
 GLT=${GLT,,} # lowercase the string
 GLT=${GLT//\&/\\&} # Add escape characters to ampersands
 GLT=${GLT//\./\\.} # Add escape characters to periods
-find /cygdrive/c/users/david.davidson/desktop/autoGlt -type f -exec \sed -i "s/href=\"\([^_#]*\)\(#.*\)*\"/href=\"\1$GLT\2\"/g" {} +
+ALTGLT=${GLT//\?/\\&} # For alternative version, trim leading ? and replace with &
+find /cygdrive/c/users/david.davidson/desktop/autoGlt -type f -exec \sed -i "s/href=\"\([^?#]*\)\(#.*\)*\"/href=\"\1$GLT\2\"/g" {} + # If the link includes an ID, add the GLT before that
+find /cygdrive/c/users/david.davidson/desktop/autoGlt -type f -exec \sed -i "s/href=\"\([^_#]*\)\(#.*\)*\"/href=\"\1$ALTGLT\2\"/g" {} + # If the link already introduces parameters with a question mark, introduce the GLT with an ampersand instead
+echo "Done. Refresh your file!"
