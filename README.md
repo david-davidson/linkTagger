a4db2604fabe7305912faa7b8b86da004e8c9486
David Davidson <david.davidson@logos.com>
2014-03-12 22:33:12 -0700

    Added support for rearranging preexisting GLT to put section IDs at the end

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-LINKTAGGER
+linkTagger
 ==========
 
 ####Automation tool for Google link tagging
 
-This bash script speeds up Google link tagging. Just drop the files you want to tag into the folder of your choice; linkTagger will prompt you for GLT parameters and take it from there!
+This bash script makes tagging links faster&mdash;way faster. Just drop the files you want to tag into the folder of your choice, and then run linkTagger from your Linux or Cygwin terminal. The script will prompt you for GLT parameters and take it from there!
+
+linkTagger knows:
+* To add GLT before section IDs, not after
+* To fix previously tagged links that put the GLT after a section ID
+* To introduce GLT with "&", not "?", when it follows preexisting link parameters that are already set off by a question mark
+* To add target="_blank"&mdash;but only after asking!
+
+That is, it&rsquo;ll tag, say, this: &lsaquo;a href="http://www.test.com#contact?p=home"&rsaquo;
+Like this: &lsaquo;a href="http://www.test.com?p=home&utm_source=var1&utm_medium=var2&utm_content=var3&utm_campaign=var4#contact" target="_blank"&rsaquo;
---
+++ linkTagger.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+MYPATH="/cygdrive/c/users/david.davidson/desktop/linkTagger" # Replace this with wherever you want to put HTML files to tag. Then drop those files into the folder, run the script from your Linux or Cygwin shell, and relax!
+echo "Enter the source:"
+read SOURCE
+SOURCE="?utm_source=$SOURCE"
+echo "Enter the medium:"
+read MEDIUM
+MEDIUM="&utm_medium=$MEDIUM"
+echo "Enter the content:"
+read CONTENT
+CONTENT="&utm_content=$CONTENT"
+echo "Enter the campaign:"
+read CAMPAIGN
+CAMPAIGN="&utm_campaign=$CAMPAIGN"
+echo "Append target=\"_blank\"? y/n"
+read ADDTARGETBLANK
+GLT="$SOURCE$MEDIUM$CONTENT$CAMPAIGN"
+GLT=${GLT,,} # lowercase the string
+GLT=${GLT//\&/\\&} # Escape the ampersands (for sed)
+GLT=${GLT//\./\\.} # Escape the periods
+ALTGLT=${GLT//\?/\\&} # Create alternative string that starts with &, not ?
+find $MYPATH -type f -exec \sed -i "s/href=\"\([^?#\"]*\)\(#[^\"]*\)*\(\?[^\"]*\)\"/href=\"\1\3\2\"/g" {} + # If a previously tagged link puts an ID before the GLT, reverse the two
+find $MYPATH -type f -exec \sed -i "s/href=\"\([^?#\"]*\)\(#[^\"]*\)*\"/href=\"\1$GLT\2\"/g" {} + # If the link includes an ID, add the GLT first
+find $MYPATH -type f -exec \sed -i "s/href=\"\([^_#\"]*\)\(#[^\"]*\)*\"/href=\"\1$ALTGLT\2\"/g" {} + # If the link already introduces other parameters with a question mark, introduce the GLT with an ampersand instead
+if [ "$ADDTARGETBLANK" = "y" ]
+	then
+		find $MYPATH -type f -exec \sed -i "s/href=\"\([^\"]*\)\">/href=\"\1\" target=\"_blank\">/g" {} +
+fi
+echo "Done. Click back to your file(s), and reload if prompted!"
