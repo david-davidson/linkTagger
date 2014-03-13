e8077dbfbd45785988165d0b7ee037a7223dc316
David Davidson <david.davidson@logos.com>
2014-03-12 23:10:29 -0700

    Improved Readme formatting

--- linkTagger.sh
+++ linkTagger.sh
@@ -18,10 +18,10 @@ GLT="$SOURCE$MEDIUM$CONTENT$CAMPAIGN"
 GLT=${GLT,,} # lowercase the string
 GLT=${GLT//\&/\\&} # Escape the ampersands (for sed)
 GLT=${GLT//\./\\.} # Escape the periods
-ALTGLT=${GLT//\?/\\&} # Create alternative string that starts with &, not ?
+ALTGLT=${GLT//\?/\\&} # Create alternative version that starts with &, not ?
 find $MYPATH -type f -exec \sed -i "s/href=\"\([^?#\"]*\)\(#[^\"]*\)*\(\?[^\"]*\)\"/href=\"\1\3\2\"/g" {} + # If a previously tagged link puts an ID before the GLT, reverse the two
 find $MYPATH -type f -exec \sed -i "s/href=\"\([^?#\"]*\)\(#[^\"]*\)*\"/href=\"\1$GLT\2\"/g" {} + # If the link includes an ID, add the GLT first
-find $MYPATH -type f -exec \sed -i "s/href=\"\([^_#\"]*\)\(#[^\"]*\)*\"/href=\"\1$ALTGLT\2\"/g" {} + # If the link already introduces other parameters with a question mark, introduce the GLT with an ampersand instead
+find $MYPATH -type f -exec \sed -i "s/href=\"\([^_#\"]*\)\(#[^\"]*\)*\"/href=\"\1$ALTGLT\2\"/g" {} + # If the link already introduces other parameters with a question mark, introduce the GLT with an ampersand instead.
 if [ "$ADDTARGETBLANK" = "y" ]
 	then
 		find $MYPATH -type f -exec \sed -i "s/href=\"\([^\"]*\)\">/href=\"\1\" target=\"_blank\">/g" {} +
