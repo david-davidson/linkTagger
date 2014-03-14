7e4c6b408fe9480f049ce5a68608a7e56c71c16a
David Davidson <david.davidson@logos.com>
2014-03-14 09:26:43 -0700

    Added smarter behavior with previously tagged links

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ This bash script makes tagging links faster&mdash;way faster. Just drop the file
 
 linkTagger knows:
 * To add GLT before section IDs, not after
-* To fix previously tagged links that put the GLT after a section ID
-* To introduce GLT with "&", not "?", when it follows preexisting link parameters that are already set off by a question mark
-* To add target="_blank"&mdash;but only after asking! #manners
+* To fix previously tagged links that put a section ID before the GLT
+* Not to add new tagging to links that are already taggd
+* To introduce GLT with &ldquo;&&rdquo;, not &rdquo;?&rdquo;, when it follows link parameters that are already set off by a question mark
+* To add target="_blank"&mdash;but only after asking!
 
 That is, it&rsquo;ll tag, say, this: 
-`<a href="http://www.test.com?#contactp=home">`
+`<a href="http://www.test.com#contact?p=home">`
 
 Like this: 
-`<a href="http://www.test.com?p=home&utm_source=var1&utm_medium=var2&utm_content=var3&utm_campaign=var4#contact" target="_blank">`
+`<a href="http://www.test.com?p=home&utm_source=var1&utm_medium=var2`
+`&utm_content=var3&utm_campaign=var4#contact" target="_blank">`
--- linkTagger.sh
+++ linkTagger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# If you encounter an error the first time you run the script, you'll need to convert the line breaks from DOS to Unix. Cd into the directory where the script lives, and strip the line breaks with this command: sed -i 's/\r//g' linkTagger.sh
-MYPATH="/cygdrive/c/users/david.davidson/desktop/linkTagger" # The script's replacement commands are recursive--they tag not only all the files in the current working directory, but all the files in all the *folders* in there, too. For that reason, the default behavior is conservative: replace MYPATH with the single directory where you'd like to tag files (not the desktop!), and put files there when you want to tag them. If you'd like to run the script in the current working directory, though, you can set MYPATH to "."
+# If you encounter an error the first time you run the script, you'll need to convert the line breaks from DOS to Unix. Cd into the directory where the script lives, and strip the problem line breaks with this command: sed -i 's/\r//g' linkTagger.sh
+MYPATH="/cygdrive/c/users/david.davidson/desktop/linkTagger" # The script's replacement commands are recursive--they tag not only all the files in the current working directory, but all the files in all the *folders* in there, too. For that reason, the recommended behavior is conservative: replace MYPATH with the single directory where you'd like to tag files (not the desktop!), and put files there when you want to tag them. If you'd like to run the script in the current working directory, though, you can set MYPATH to "."
 echo "Enter the source:"
 read SOURCE
 echo "Enter the medium:"
@@ -21,9 +21,10 @@ GLT=${GLT//\&/\\&} # Escape the ampersands
 GLT=${GLT//\./\\.} # Escape the periods
 ALTGLT=${GLT//\?/\\&} # Create alternative version that starts with &, not ?
 find $MYPATH -type f -exec \sed -i "s/href=\"\([^?#\"]*\)\(#[^\"]*\)*\(\?[^\"]*\)\"/href=\"\1\3\2\"/g" {} + # If a previously tagged link puts an ID before the GLT, reverse the two
-find $MYPATH -type f -exec \sed -i "s/href=\"\([^?#\"]*\)\(#[^\"]*\)*\"/href=\"\1$GLT\2\"/g" {} + # If the link includes an ID, add the GLT first
-find $MYPATH -type f -exec \sed -i "s/href=\"\([^_#\"]*\)\(#[^\"]*\)*\"/href=\"\1$ALTGLT\2\"/g" {} + # If the link already introduces other parameters with a question mark, introduce the GLT with an ampersand instead
-# Even better: this works in Sublime; how to get it to work in sed? href="([^?#"]*)\?(?!.*?utm_).*?([^#]*?)*?"
+find $MYPATH -type f -exec \sed -i "s/href=\"\([^?#\"]*\)\(#[^\"]*\)*\"/href=\"\1$GLT\2\"/g" {} + # Tag all the links that don't include "?"; if there's a section ID, put the GLT before it.
+find $MYPATH -type f -exec \sed -i "s/href=\"\([^\"]*utm_[^\"]*\)\"/href= \"\1\"/g" {} + # "Comment out" previously tagged links. (Sed doesn't support negative lookaheads, so we can't do something like [ href="([^?#"]*)\?(?!.*?utm_).*?([^#]*?)*?" ]; instead, we add [and later remove] a space after "href=", which addition hides matching links from the next regex. [If the undo somehow fails, browsers still render links with a space after "href=".])
+find $MYPATH -type f -exec \sed -i "s/href=\"\([^#\"]*\)\(#[^\"]*\)*\"/href=\"\1$ALTGLT\2\"/g" {} + # Tag links that (1) include "?" and (2) don't have any GLT; introduce the GLT with "&", not "?".
+find $MYPATH -type f -exec \sed -i "s/href= \"\([^\"]*utm_[^\"]*\)\"/href=\"\1\"/g" {} + # Remove that space after "href=" in previously tagged links.
 if [ "$ADDTARGETBLANK" = "y" ]
 	then
 		find $MYPATH -type f -exec \sed -i "s/href=\"\([^\"]*\)\">/href=\"\1\" target=\"_blank\">/g" {} +
