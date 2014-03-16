98aa91be4c3710a9efab82fba6bd2a79b332831d
David Davidson <david.davidson@logos.com>
2014-03-16 16:32:21 -0700

    Made output more verbose

--- linkTagger.sh
+++ linkTagger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Two-step setup:
-# 1. Change MYPATH (line 8) to the folder where you'd like to tag files. Note that the script tags not only all the files in the chosen directory, but also all the files in *all the directories* in there, too. (Of course, if you'd like to run the script in the current working directory, you can set MYPATH to "."--just don't run it in, say, the desktop!)
+# 1. Change MYPATH (line 8) to the directory where you'd like to tag your files. Note that the script tags not only all the files in the chosen directory, but also all the files in *all the directories* in there, too. (Of course, if you'd like to run the script in the current working directory, you can set MYPATH to "."--just don't run it in, say, the desktop!)
 # 2. If you encounter an error the first time you run the script, you probably need to convert the line endings. Cd into the directory where the script lives, and run this command: "sed -i 's/\r//g' linkTagger.sh"
 # Oh, and you might as well set up an alias to run this: "alias glt='absolute/path/to/script/linkTagger.sh'" or something
 
@@ -31,9 +31,11 @@ GLT=${GLT//&/\\&} # Escape the ampersands
 GLT=${GLT////\\/} # Escape slashes
 GLT=${GLT// /} # Remove spaces, just in case
 printf "\nAppend target=\"_blank\"? y/n\n" ; read ADDTARGETBLANK
+echo "Checking link structure..."
 find "$MYPATH" -type f -exec \sed -i "s/a\(.*\?\)href=\"\([^?#\"]*\)\(#[^\"]*\)*\(?[^\"]*\)\"/a\1href=\"\2\4\3\"/g" {} + # Move all section IDs to the end of their links
 if [[ "$TAG" = "true" ]]
 	then
+	echo "Tagging links..."
 	find "$MYPATH" -type f -exec \sed -i "s/a\(.*\?\)href=\"\([^?#\"]*\)\(#[^\"]*\)*\"/a\1href=\"\2?$GLT\3\"/g" {} + # Tag all the links that don't include "?"; introduce the GLT with "?"; if there's a section ID, keep it at the end
 	find "$MYPATH" -type f -exec \sed -i "s/href=\"\([^\"]*utm_[^\"]*\)\"/href= \"\1\"/g" {} + # What about links that include "?" but don't include GLT? sed doesn't support negative lookahead, so we'll "comment out" tagged links by adding a space after href=, which we'll later remove...
 	find "$MYPATH" -type f -exec \sed -i "s/a\(.*\?\)href=\"\([^#\"]*\)\(#[^\"]*\)*\"/a\1href=\"\2\&$GLT\3\"/g" {} + # ...tag remaining links, which (1) don't have any GLT and (2) include "?" (so we introduce the GLT with "&")...
@@ -41,6 +43,7 @@ if [[ "$TAG" = "true" ]]
 fi
 if [[ "$ADDTARGETBLANK" = "y" || "$ADDTARGETBLANK" = "Y" ]]
 	then
+	echo "Adding target=\"_blank\"..."
 	find "$MYPATH" -type f -exec \sed -i "s/a\(.*\?\)href=\"\([^\"]*\)\">/a\1href=\"\2\" target=\"_blank\">/g" {} + # Append target="_blank"
 fi
 printf "\nDone! Click back to your file(s), and reload if prompted.\n"
